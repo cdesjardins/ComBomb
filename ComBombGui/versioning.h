100306cc52b44b8b097d04c862c117f9dbd5947a
Chris Desjardins <chris.desjardins@amscins.com>
2013-10-12 19:44:27 +0200

    New version

--- ComBombGui/versioning.h
+++ ComBombGui/versioning.h
@@ -1,3 +1,3 @@
 /* This is a generated file, do not change, see vergen.py   */
 /* Do not include this file directly, use include/version.h */
-#define CB_GIT_VER_STR "v0.1a-8-gdd42a9b-dirty"
+#define CB_GIT_VER_STR "v0.1a-9-gd73011c-dirty"
