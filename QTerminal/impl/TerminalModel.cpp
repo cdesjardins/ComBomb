594469e0bb4cbe2e0039b287ace74a199dcb6bf6
Chris Desjardins <git@chrisd.info>
2017-01-04 18:24:08 +0100

    Update capture timestamp to not use put_time

--- QTerminal/impl/TerminalModel.cpp
+++ QTerminal/impl/TerminalModel.cpp
@@ -280,8 +280,24 @@ void TerminalModel::suppressOutput(bool suppress)
 
 void TerminalModel::writeTimeStampToCaptureFile()
 {
-    std::time_t t = std::time(nullptr);
-    _captureFile << std::put_time(std::localtime(&t), "%D %T ");
+#ifdef WIN32
+#define localtime_r(_Time, _Tm) localtime_s(_Tm, _Time)
+#endif
+    tm localTime;
+    std::chrono::system_clock::time_point t = std::chrono::system_clock::now();
+    time_t now = std::chrono::system_clock::to_time_t(t);
+    localtime_r(&now, &localTime);
+
+    const std::chrono::duration<double> tse = t.time_since_epoch();
+    std::chrono::seconds::rep milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(tse).count() % 1000;
+
+    _captureFile << (1900 + localTime.tm_year) << '-'
+        << std::setfill('0') << std::setw(2) << (localTime.tm_mon + 1) << '-'
+        << std::setfill('0') << std::setw(2) << localTime.tm_mday << ' '
+        << std::setfill('0') << std::setw(2) << localTime.tm_hour << ':'
+        << std::setfill('0') << std::setw(2) << localTime.tm_min << ':'
+        << std::setfill('0') << std::setw(2) << localTime.tm_sec << '.'
+        << std::setfill('0') << std::setw(3) << milliseconds << " ";
 }
 
 void TerminalModel::addToCaptureFile(const char* buf, int len)
