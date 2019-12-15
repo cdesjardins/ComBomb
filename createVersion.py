9198f27470107fc07f95b4e2dd6e95a4b5d4eb0f
Chris Desjardins <chris@ridedott.com>
2019-12-15 16:44:07 +0100

    Update to python 3

--- createVersion.py
+++ createVersion.py
@@ -19,6 +19,7 @@
 #
 
 import sys, os
+import traceback
 from subprocess import Popen, PIPE
 
 def touch(fname, times=None):
@@ -36,26 +37,27 @@ class CreateVer:
     def run(self):
         gitVerStr = self.getVerStr()
         f = open("v.h", "wb")
-        f.write("/*\n\
-    ComBomb - Terminal emulator\n\
-    Copyright (C) 2014  Chris Desjardins\n\
-    http://blog.chrisd.info cjd@chrisd.info\n\
-\n\
-    This program is free software: you can redistribute it and/or modify\n\
-    it under the terms of the GNU General Public License as published by\n\
-    the Free Software Foundation, either version 3 of the License, or\n\
-    (at your option) any later version.\n\
-\n\
-    This program is distributed in the hope that it will be useful,\n\
-    but WITHOUT ANY WARRANTY; without even the implied warranty of\n\
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the\n\
-    GNU General Public License for more details.\n\
-\n\
-    You should have received a copy of the GNU General Public License\n\
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\
-*/\n");
-        f.write("/* This is a generated file, do not change, see " + os.path.basename(__file__) + "   */\n")
-        f.write("#define CB_GIT_VER_STR \"" + gitVerStr + "\"\n")
+        f.write(b"""/*
+    ComBomb - Terminal emulator
+    Copyright (C) 2014  Chris Desjardins
+    http://blog.chrisd.info cjd@chrisd.info
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+""");
+        f.write(b"/* This is a generated file, do not change, see " + str.encode(os.path.basename(__file__)))
+        f.write(b"   */\n#define CB_GIT_VER_STR \"" + gitVerStr + b"\"\n")
         f.close()
         return gitVerStr
 
@@ -66,4 +68,5 @@ if __name__ == "__main__":
         CreateVer.run();
     except Exception as e:
         print(e)
+        traceback.print_tb(e.__traceback__)
         print("Check to make sure git is in the path")
